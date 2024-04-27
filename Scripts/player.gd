b42fe5594b0012aaa08ece52a4a9556d1018beb4
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2024-04-27 12:59:19 +0100

    Implement basic functional movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,8 @@ extends RigidBody3D
 @onready var camHolder: Node3D = $CamHolder
 @onready var cam: Camera3D = $CamHolder/Camera3D
 
-const SPEED = 100
+const FORCE = 100
+const MAXSPEED = 30
 const SENSITIVITY = 0.01
 
 # Called when the node enters the scene tree for the first time.
@@ -15,14 +16,23 @@ func updateCamera():
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseMotion: # Control camera rotation
-		print(-event.relative.x)
-		rotate_y(-event.relative.x * SENSITIVITY)
+		camHolder.rotate_y(-event.relative.x * SENSITIVITY)
 		cam.rotate_x(-event.relative.y * SENSITIVITY)
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func speedControl():
+	var speed: Vector3 = Vector3(linear_velocity.x, 0, linear_velocity.z)
+	print(speed)
+	if (speed.length() > MAXSPEED):
+		var limited = speed.normalized()*MAXSPEED
+		linear_velocity = Vector3(limited.x, linear_velocity.y, limited.z)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var movevector: Vector2 = Input.get_vector("Back", "Forward", "Left", "Right")
-	apply_force(Vector3(movevector.x*SPEED, 0, movevector.y*SPEED))
+	apply_force(camHolder.transform.basis*Vector3(movevector.y*FORCE, 0, -movevector.x*FORCE))
+	speedControl()
